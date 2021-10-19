a7e8f72c4eecf5375b1caf3e0be2591632db83c1
JValero1 <92802906+JValero1@users.noreply.github.com>
2021-10-19 10:38:21 -0400

    Ejercicio practico #1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+         //numero: 178   
+         byte a = 1;
+		 byte b = 7;
+		 byte c = 8;
+         
+	 	 //aacb
+		 Console.WriteLine("numero 1: " + a + a + c + b);
+
+		 Console.WriteLine("----");
+
+         //bca
+		 Console.WriteLine("numero 2: " + b + c + a);
+
+	   	 Console.WriteLine("----");
+ 
+		 //bcbc
+	 	 Console.WriteLine("numero 3: " + b + c + b + c);
+        
+        
+        }
+    }
+}
